f3e2aedc3125ebe95cbd40e1cadc298fd52130e7
Evecandy <evecandymutunga@gmail.com>
2023-05-18 17:08:00 +0300

    Added Homepage

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,55 @@
 import './App.css'
 import Footer from './components/Footer'
 import NavBar from './components/NavBar'
+import NotFound from './components/NotFound'
+import Homepage from './components/Homepage'
 
 import SideBar from './components/Sidebar'
-import Content from './components/Content'
+import Content from './pages/Content'
+
+const data = [{
+Author: "Eve",
+BlogName: "Food",
+Description: "Eating healthy"
+
+},
+{
+Author: "Candy",
+BlogName: "Movies",
+Description: "Futurama: The Odd Couple"
+},
+{
+  Author: "Ray",
+BlogName: "Music",
+Description: "The Jonas Brothers"
+}
+
+]
 
 
 function App() {
 
   return (
     <>
+    <div>
        <div className="gridContainer">
       
-      <div className="NavBar">
-      <NavBar />
-     </div>
+      <NavBar title={data.title}/>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Homepage data={data}/>}></Route>
+          <Route path="/details" element={<Content/>}></Route>
 
-      <div className="SideBar">
-      <SideBar />
-      </div>
-      <div className="Content">
-      <Content />
-      </div>
+          <Route path="a"element={<PageNotFound/>}></Route>
+        </Routes>
+
+        </BrowserRouter>
+        <Footer name={data.name} date={data.date}/>
+     </div>
       <div className="Footer">
       <Footer />
       </div>
+
     </div>
 
     </>
---
+++ src/components/Homepage.jsx
@@ -0,0 +1,5 @@
+function Home () {
+    return (<div><h1>Welcome to my Blog</h1></div>);
+  }
+  
+  export default Home;
